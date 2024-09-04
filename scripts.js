ef98b5fba062b738ff9366ad514180bf7f5831ea
danieljuvito <100745516+danieljuvito@users.noreply.github.com>
2024-09-04 16:31:34 +0700

    Add side nav

--- scripts.js
+++ scripts.js
@@ -1,24 +1,26 @@
-window.addEventListener('resize', function () {
-    const movingElement = document.querySelector('#order-button');
-    const containerLarge = document.querySelector('#container-large');
-    const containerSmall = document.querySelector('#container-small');
+function handleBuyButton() {
+    window.addEventListener('resize', function () {
+        const movingElement = document.querySelector('#order-button');
+        const containerLarge = document.querySelector('#container-large');
+        const containerSmall = document.querySelector('#container-small');
 
-    if (window.innerWidth <= 1000) {
-        if (movingElement.getAttribute("id") !== containerSmall.children[0].getAttribute("id")) {
-            containerSmall.insertBefore(movingElement, containerSmall.firstChild);
+        if (window.innerWidth <= 1000) {
+            if (movingElement.getAttribute("id") !== containerSmall.children[0].getAttribute("id")) {
+                containerSmall.insertBefore(movingElement, containerSmall.firstChild);
+            }
+        } else {
+            containerLarge.appendChild(movingElement);
         }
-    } else {
-        containerLarge.appendChild(movingElement);
-    }
-});
+    });
 
-// Initial check
-if (window.innerWidth <= 1000) {
-    const container = document.querySelector('#container-small');
-    container.insertBefore(document.querySelector('#order-button'), container.firstChild);
+    // Initial check
+    if (window.innerWidth <= 1000) {
+        const container = document.querySelector('#container-small');
+        container.insertBefore(document.querySelector('#order-button'), container.firstChild);
+    }
 }
 
-window.onload = function () {
+function handleLanguageSelector() {
     const htmlTag = document.querySelector("html")
     const idLangButton = document.querySelector("#id-button")
     const enLangButton = document.querySelector("#en-button")
@@ -45,4 +47,27 @@ window.onload = function () {
         idLangButton.classList.remove("active")
         enLangButton.classList.add("active")
     })
+}
+
+function handleSideNav() {
+    const menuBarButton = document.querySelector("#menu-bar")
+    const nav = document.querySelector("nav")
+    let isOpen = false
+    menuBarButton.addEventListener("click", () => {
+        nav.style.left = "0px";
+        isOpen = true
+        nav.focus()
+    })
+    nav.addEventListener("focusout", () => {
+        if (isOpen) {
+            nav.style.left = "-200px";
+            isOpen = false
+        }
+    })
+}
+
+window.onload = function () {
+    handleBuyButton()
+    handleLanguageSelector()
+    handleSideNav()
 }
